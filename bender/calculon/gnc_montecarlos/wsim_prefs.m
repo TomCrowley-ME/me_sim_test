ae5035a50aa9fa29bab6c46dbac4c4965a61d7ab
markagbayani <markagbayani@moonexpress.com>
2016-01-28 20:08:45 -0800

    Mods to wsim_prefs.m used for Monte Carlo runs

--- bender/calculon/gnc_montecarlos/wsim_prefs.m
+++ bender/calculon/gnc_montecarlos/wsim_prefs.m
@@ -1,5 +1,7 @@
 pref_plot_test_data       = 0;
 pref_plot_attitude_errors = 0;
+pref_plot_phase_plane_c   = 0;
+pref_plot_phase_plane_v   = 0;
 pref_plot_phase_plane     = 0;
 pref_plot_prop_data       = 0;
 pref_export_to_ppt        = 0;
