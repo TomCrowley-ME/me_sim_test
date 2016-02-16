4970b0a8f5d2c84a99d588d31b058fcdd7cc09b1
Jim Kaidy <jimkaidy@moonexpress.com>
2016-02-16 15:32:27 -0500

    Sets limits and gains for both velocity and acceleration errors in Targeting Burn guidance

---
+++ bender/phil/wsim/fsw/gdn/gal/csu_lat_landing_approach_targeting_init.m
@@ -0,0 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright 2010 - 2016 Moon Express, Inc.
+% All Rights Reserved.
+%
+% PROPRIETARY DATA NOTICE:
+% The data herein include Proprietary Data and are restricted under the
+% Data Rights provisions of Lunar CATALYST Space Act Agreement
+% No. SAAM ID# 18251 and Reimbursable Space Act Agreement No.SAA2-402930.
+% All information contained herein is and remains proprietary to and the
+% property of Moon Express, Inc. Dissemination of this information or
+% reproduction of this material is strictly forbidden unless prior
+% written permission is obtained from Moon Express, Inc.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+gdn_targeting_velocity_error_limit = [10.0 10.0 30.0];         % [m/sec]
+gdn_targeting_lateral_accel_limit  = [2.0  2.0];               % [m/sec^2]
+
+gdn_targeting_velocity_error_gain  = [1.4  1.4  2.0];
+gdn_targeting_lateral_accel_gain   = [0.5  0.5];
