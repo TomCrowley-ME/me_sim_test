40e9ab002b12679936cd911b26abb69cd233a221
jimkaidy <jkaidy@localhost.localdomain>
2016-03-10 07:52:38 -0500

    Fixed star tracker noise to correct units from arc-sec to radians.  Single case run now more user friendly.

--- bender/calculon/gnc_montecarlos/single_case_rerun.m
+++ bender/calculon/gnc_montecarlos/single_case_rerun.m
@@ -1,5 +1,5 @@
 % Single case rerun
-load('doi_to_touchdown_mc73.mat')
+%load('doi_to_touchdown_mc73.mat')
 sim('nimbus');
 cd /home/jkaidy/me_sim/bender/phil/wsim/scenarios/scenario_3_deorbit_to_landing/scn3_wsim_data/;
 process_test_data
